f37d01c910baada595d508884d23760998249714
Marek Goldmann <marek.goldmann@gmail.com>
2015-06-26 11:01:24 +0200

    Make the setuptools script use README.rst

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from setuptools import setup, find_packages
+import codecs
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
@@ -14,7 +15,7 @@ setup(
     description = 'Simple command line tool for executing Containers Testing Framework',
     license='GPLv2',
     keywords = 'docker behave',
-    long_description = "Containers Testing Framework (CTF) is a simple wrapper around Behave testing framework. Behave is a promising approach for testing containers, since it enables one to focus on describing the behavior of the container from a High Level and in simple English. For more information on how to use Behave and how to write tests using Behave, please refer to the Behave project page.",
+    long_description = codecs.open('README.rst', encoding="utf8").read(),
     entry_points = {
         'console_scripts': ['ctf-cli=ctf_cli.cli_runner:run'],
     },
